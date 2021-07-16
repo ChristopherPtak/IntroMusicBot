55b5a818f5b02f711130477bf30bce07cdd8a68a
Christopher Ptak <cptak@comcast.net>
2021-07-15 23:39:13 -0400

    Created minimal bot that does nothing

--- bot.js
+++ bot.js
@@ -1,3 +1,25 @@
 
+const fs = require('fs');
+const Discord = require('discord.js');
 
+// Create resources
+
+const client = new Discord.Client();
+
+// Set event handlers
+
+client.on('ready', function () {
+    console.log('Started IntroMusicBot');
+});
+
+// Start bot
+
+fs.readFile('token.txt', 'utf8', function (err, token) {
+    if (err) {
+        console.error('Unable to open token.txt');
+        client.destroy();
+    } else {
+        client.login(token);
+    }
+});
 
